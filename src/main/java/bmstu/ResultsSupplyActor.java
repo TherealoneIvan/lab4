f8b224d5a07d9b07597ee0d1510925922db8bc32
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-14 21:29:10 +0300

    Scripted auto-commit on change (2020-11-14 21:29:10) by gitwatch.sh

--- src/main/java/bmstu/ResultsSupplyActor.java
+++ src/main/java/bmstu/ResultsSupplyActor.java
@@ -2,8 +2,10 @@ package bmstu;
 
 import akka.actor.AbstractActor;
 
+import java.util.Map;
+
 public class ResultsSupplyActor extends AbstractActor {
-    
+    private Map<String , String> store
     public Receive createReceive() {
         return receiveBuilder()
                 .match(
