6c5af2b8b978216fe03723c9c3a7503daf17937c
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-14 22:04:49 +0300

    Scripted auto-commit on change (2020-11-14 22:04:48) by gitwatch.sh

--- src/main/java/bmstu/ResultsSupplyActor.java
+++ src/main/java/bmstu/ResultsSupplyActor.java
@@ -10,7 +10,9 @@ public class ResultsSupplyActor extends AbstractActor {
 
     private Map<String , ArrayList<String>> store = new HashMap<>();
     private void resultStoring(JavaScriptFunctionClass item , Map<String , ArrayList<String>> myStore){
-        if 
+        if (myStore.containsKey(item.getPackageID())){
+            myStore.put(item.getPackageID() , item.getFunctionRes());
+        }
     }
     private void packageIdPrinter(String packageID){
         for (int i = store.size() ; i < ; i++) {
