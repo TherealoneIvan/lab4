c7d8d4b795ca1b664e8f91ae1bc0a5ef57e26696
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-14 21:50:56 +0300

    Scripted auto-commit on change (2020-11-14 21:50:56) by gitwatch.sh

--- src/main/java/bmstu/ResultsSupplyActor.java
+++ src/main/java/bmstu/ResultsSupplyActor.java
@@ -17,6 +17,7 @@ public class ResultsSupplyActor extends AbstractActor {
                         }
                 )
                 .match(
+                        String.class,
                         
                 )
                 .build();
