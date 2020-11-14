43e239c1c269e06302c41c381a86469c8dc6e294
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-14 23:31:10 +0300

    Scripted auto-commit on change (2020-11-14 23:31:10) by gitwatch.sh

--- src/main/java/bmstu/JSTestExecutor.java
+++ src/main/java/bmstu/JSTestExecutor.java
@@ -2,9 +2,19 @@ package bmstu;
 
 import akka.actor.AbstractActor;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 
 public class JSTestExecutor extends AbstractActor {
-    public String jsExecutor()
+    public String jsExecutor(){
+        ScriptEngine engine = new
+                ScriptEngineManager().getEngineByName("nashorn");
+        engine.eval(jscript);
+        Invocable invocable = (Invocable) engine;
+        return invocable.invokeFunction(functionName, params).toString();
+    }
     @Override
     public Receive createReceive() {
         return receiveBuilder()
