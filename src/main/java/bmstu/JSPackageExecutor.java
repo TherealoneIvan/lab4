ac4a4f4888e5d66cdcb02e52855891f1a56f1047
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-14 21:05:42 +0300

    Scripted auto-commit on change (2020-11-14 21:05:42) by gitwatch.sh

--- src/main/java/bmstu/JSPackageExecutor.java
+++ src/main/java/bmstu/JSPackageExecutor.java
@@ -1,2 +1,12 @@
-package bmstu;public class JSPackageExecutor {
+package bmstu;
+
+import akka.actor.AbstractActor;
+
+
+public class JSPackageExecutor extends AbstractActor {
+    
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
