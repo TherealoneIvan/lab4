0e28857461e8e4ce9620fb8e551d2e4c98944f2c
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-14 21:46:48 +0300

    Scripted auto-commit on change (2020-11-14 21:46:48) by gitwatch.sh

--- src/main/java/bmstu/JavaScriptFunctionClass.java
+++ src/main/java/bmstu/JavaScriptFunctionClass.java
@@ -1,2 +1,26 @@
-package bmstu;public class JavaScriptFunctionClass {
+package bmstu;
+
+public class JavaScriptFunctionClass {
+    private String packageID , functionRes;
+
+    public JavaScriptFunctionClass(String packageID, String functionRes) {
+        this.packageID = packageID;
+        this.functionRes = functionRes;
+    }
+
+    public String getPackageID() {
+        return packageID;
+    }
+
+    public void setPackageID(String packageID) {
+        this.packageID = packageID;
+    }
+
+    public String getFunctionRes() {
+        return functionRes;
+    }
+
+    public void setFunctionRes(String functionRes) {
+        this.functionRes = functionRes;
+    }
 }
