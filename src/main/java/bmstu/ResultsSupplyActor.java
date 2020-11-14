7f4bb34ff331fd8d1b6dd2a1f21f1dfa48b22874
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-14 21:45:13 +0300

    Scripted auto-commit on change (2020-11-14 21:45:13) by gitwatch.sh

--- src/main/java/bmstu/ResultsSupplyActor.java
+++ src/main/java/bmstu/ResultsSupplyActor.java
@@ -6,14 +6,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ResultsSupplyActor extends AbstractActor {
-    public static String functionResReceive(String functionResult , String packageID){
-        return functionResult;
-    }
+
     private Map<String , String> store = new HashMap<>();
     public Receive createReceive() {
         return receiveBuilder()
                 .match(
-
+                        
                 )
                 .build();
     }
