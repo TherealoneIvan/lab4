a805aa8a13e37514f8dd8b57805b78cd20a9ec5c
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-14 23:24:38 +0300

    Scripted auto-commit on change (2020-11-14 23:24:37) by gitwatch.sh

--- src/main/java/bmstu/JSTestExecutor.java
+++ src/main/java/bmstu/JSTestExecutor.java
@@ -4,7 +4,7 @@ import akka.actor.AbstractActor;
 
 
 public class JSTestExecutor extends AbstractActor {
-
+    public static 
     @Override
     public Receive createReceive() {
         return receiveBuilder()
