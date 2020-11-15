afe48f0d43e1507d832cedabbfee3c1f1f8b4123
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-15 23:09:43 +0300

    Scripted auto-commit on change (2020-11-15 23:09:43) by gitwatch.sh

--- src/main/java/bmstu/ActorRouter.java
+++ src/main/java/bmstu/ActorRouter.java
@@ -3,5 +3,11 @@ package bmstu;
 import akka.actor.AbstractActor;
 
 public class ActorRouter extends AbstractActor {
+    private ResultsSupplyActor storeActor;
+    private JSTestExecutorActor testExecutorActor;
     
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
