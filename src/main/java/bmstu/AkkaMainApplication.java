271288485efbfab7f8306b18775a6cd243a15ed4
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-15 22:52:26 +0300

    Scripted auto-commit on change (2020-11-15 22:52:25) by gitwatch.sh

--- src/main/java/bmstu/AkkaMainApplication.java
+++ src/main/java/bmstu/AkkaMainApplication.java
@@ -4,12 +4,15 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 
-public class AkkaMainApplication {
-    public static void main(String[] args) throws IOException {
-    ActorSystem system = ActorSystem.create("test");
-    Props propsJsTestExecProp = Props.create(JSTestExecutorActor.class);
-    Props propsResultsSupplyProp = Props.create(ResultsSupplyActor.class);
-    ActorRef supplyActor = system.actorOf(propsResultsSupplyProp);
-    ActorRef executorJsActor = system.actorOf(propsJsTestExecProp);
+import java.io.IOException;
 
+public class AkkaMainApplication {
+
+    public static void main(String[] args) throws IOException {
+        ActorSystem system = ActorSystem.create("test");
+        Props propsJsTestExecProp = Props.create(JSTestExecutorActor.class);
+        Props propsResultsSupplyProp = Props.create(ResultsSupplyActor.class);
+        ActorRef supplyActor = system.actorOf(propsResultsSupplyProp);
+        ActorRef executorJsActor = system.actorOf(propsJsTestExecProp);
+    }
 }
