fa672b2d6ebcb0b3a9bb6a7f6327f6d03d411195
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-16 01:06:39 +0300

    Scripted auto-commit on change (2020-11-16 01:06:39) by gitwatch.sh

--- src/main/java/bmstu/AkkaMainApplication.java
+++ src/main/java/bmstu/AkkaMainApplication.java
@@ -3,6 +3,7 @@ package bmstu;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.stream.ActorMaterializer;
 import akka.stream.Materializer;
 
 import java.io.IOException;
@@ -12,6 +13,6 @@ public class AkkaMainApplication {
     public static void main(String[] args) throws IOException {
         final ActorSystem system = ActorSystem.create("test");
         ActorRouter router = new ActorRouter(system);
-        final Materializer materializer = 
+        final Materializer materializer = ActorMaterializer.create(system);
     }
 }
