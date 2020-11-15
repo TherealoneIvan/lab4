1a34b23e49712139ad178a2629c3d2c1b417d8a6
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-15 22:00:58 +0300

    Scripted auto-commit on change (2020-11-15 22:00:58) by gitwatch.sh

--- src/main/java/bmstu/AkkaMainApplication.java
+++ src/main/java/bmstu/AkkaMainApplication.java
@@ -8,5 +8,7 @@ public class AkkaMainApplication {
     ActorSystem system = ActorSystem.create("test");
     Props propsJsTestExecProp = Props.create(JSTestExecutorActor.class);
     Props propsResultsSupplyProp = Props.create(ResultsSupplyActor.class);
-    ActorRef supplyActor = system.cre
+    ActorRef supplyActor = system.actorOf(propsResultsSupplyProp);
+    ActorRef executorJsActor = system.actorOf(propsJsTestExecProp);
+    
 }
