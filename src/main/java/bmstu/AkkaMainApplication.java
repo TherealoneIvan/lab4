5adbc51b134caa2492acf30f89b40f5cfb4d5dc2
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-16 01:23:18 +0300

    Scripted auto-commit on change (2020-11-16 01:23:18) by gitwatch.sh

--- src/main/java/bmstu/AkkaMainApplication.java
+++ src/main/java/bmstu/AkkaMainApplication.java
@@ -1,14 +1,7 @@
 package bmstu;
 
-import akka.NotUsed;
-import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
-import akka.actor.Props;
-import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
-import akka.http.javadsl.ServerBinding;
-import akka.http.javadsl.model.HttpRequest;
-import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.Materializer;
 
@@ -22,13 +15,6 @@ public class AkkaMainApplication {
         ActorRouter router = new ActorRouter(system);
         final Http http = Http.get(system);
         final Materializer materializer = ActorMaterializer.create(system);
-        MainHttp instance = new MainHttp(system);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
-                instance.createRoute(system).flow(system, materializer);
-        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
-                routeFlow,
-                ConnectHttp.toHost("localhost", 8080),
-                materializer
-        );
+        
     }
 }
