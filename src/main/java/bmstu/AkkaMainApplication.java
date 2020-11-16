46d7b189a6aa0114896a07dfd8a4d7acb32704a4
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-16 03:17:10 +0300

    Scripted auto-commit on change (2020-11-16 03:17:10) by gitwatch.sh

--- src/main/java/bmstu/AkkaMainApplication.java
+++ src/main/java/bmstu/AkkaMainApplication.java
@@ -7,6 +7,7 @@ import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.AllDirectives;
 import akka.stream.ActorMaterializer;
 import akka.stream.Materializer;
 import akka.stream.javadsl.Flow;
@@ -14,12 +15,13 @@ import akka.stream.javadsl.Flow;
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 
-public class AkkaMainApplication {
+public class AkkaMainApplication extends AllDirectives {
 
     public static void main(String[] args) throws IOException {
         final ActorSystem system = ActorSystem.create("test");
         ActorRouter router = new ActorRouter(system);
         final Http http = Http.get(system);
+        
         final Materializer materializer = ActorMaterializer.create(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
                 instance.createRoute(system).flow(system, materializer);
