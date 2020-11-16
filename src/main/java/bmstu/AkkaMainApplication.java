8a6a2ce648cdcc725179efacbc125ef7f1cbe1b9
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-16 19:38:22 +0300

    Scripted auto-commit on change (2020-11-16 19:38:22) by gitwatch.sh

--- src/main/java/bmstu/AkkaMainApplication.java
+++ src/main/java/bmstu/AkkaMainApplication.java
@@ -36,4 +36,5 @@ public class AkkaMainApplication extends AllDirectives {
                 .thenCompose(ServerBinding::unbind)
                 .thenAccept(unbound -> system.terminate());
     }
+    
 }
