82540c32f7464e325437411a6de9c9d1aff671e7
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-16 22:28:54 +0300

    Scripted auto-commit on change (2020-11-16 22:28:54) by gitwatch.sh

--- src/main/java/bmstu/JsonJsPackage.java
+++ src/main/java/bmstu/JsonJsPackage.java
@@ -6,6 +6,6 @@ public class JsonJsPackage {
     public String packageId;
     public String jsCode;
     public String funcName;
-    public ArrayList<JavaScriptFunctionStore> Test;
-    
+    public ArrayList<JavaScriptFunctionStore> test;
+
 }
