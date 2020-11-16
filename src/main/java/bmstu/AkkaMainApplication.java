fe8898130876acce5ffc98eefa43740ac468b497
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-16 03:14:22 +0300

    Scripted auto-commit on change (2020-11-16 03:14:22) by gitwatch.sh

--- src/main/java/bmstu/AkkaMainApplication.java
+++ src/main/java/bmstu/AkkaMainApplication.java
@@ -2,7 +2,9 @@ package bmstu;
 
 import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
@@ -19,6 +21,17 @@ public class AkkaMainApplication {
         ActorRouter router = new ActorRouter(system);
         final Http http = Http.get(system);
         final Materializer materializer = ActorMaterializer.create(system);
-        
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute(system).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
     }
 }
