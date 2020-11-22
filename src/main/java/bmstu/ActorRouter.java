8ad544c30dafe2de6abcf2ee3f44a15b63b273f1
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-23 00:57:44 +0300

    Scripted auto-commit on change (2020-11-23 00:57:44) by gitwatch.sh

--- src/main/java/bmstu/ActorRouter.java
+++ src/main/java/bmstu/ActorRouter.java
@@ -4,15 +4,13 @@ import akka.actor.*;
 import akka.routing.RoundRobinPool;
 
 public class ActorRouter extends AbstractActor {
-    private ActorRef storeActor;
-    private ActorRef testExecutorActor;
-    Props propsJsTestExecProp = Props.create(JSTestExecutorActor.class);
-    Props propsResultsSupplyProp = Props.create(ResultsSupplyActor.class);
-    storeActor = system.actorOf(propsResultsSupplyProp , "Store");
-    testExecutorActor = system.actorOf(
+    private ActorRef storeActor = .actorOf(propsResultsSupplyProp , "Store");
+    private ActorRef testExecutorActor = .actorOf(
             new RoundRobinPool(5)
                     .props(propsJsTestExecProp)
         );
+    Props propsJsTestExecProp = Props.create(JSTestExecutorActor.class);
+    Props propsResultsSupplyProp = Props.create(ResultsSupplyActor.class);
     public String makeRequest(String packageId){
         return packageId;
     }
