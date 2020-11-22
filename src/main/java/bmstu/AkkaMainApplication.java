3c5f761845e25e5b8bdeeadbf50bde1aed87207f
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-22 23:54:03 +0300

    Scripted auto-commit on change (2020-11-22 23:54:03) by gitwatch.sh

--- src/main/java/bmstu/AkkaMainApplication.java
+++ src/main/java/bmstu/AkkaMainApplication.java
@@ -49,7 +49,7 @@ public class AkkaMainApplication extends AllDirectives {
                      return complete("Test started!");
                  }));
                  get(()->{
-                     Future<Object> result = Patterns.ask
+                     Future<Object> result = Patterns.ask(router , )
                          }
 
                  )
