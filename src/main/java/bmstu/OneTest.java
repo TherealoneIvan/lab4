745167488ad3eceaf501cb7548d323406ea504da
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-23 23:23:09 +0300

    Scripted auto-commit on change (2020-11-23 23:23:09) by gitwatch.sh

--- src/main/java/bmstu/OneTest.java
+++ src/main/java/bmstu/OneTest.java
@@ -8,4 +8,12 @@ public class OneTest {
     private  ArrayList<Integer> params;
     private String function;
     private String packageId;
+
+    public OneTest(String testName, String expRes, ArrayList<Integer> params, String function, String packageId) {
+        this.testName = testName;
+        this.expRes = expRes;
+        this.params = params;
+        this.function = function;
+        this.packageId = packageId;
+    }
 }
