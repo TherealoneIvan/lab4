420434c1f3c4aa2d3ca17b54687b24dc8842e949
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-23 20:24:07 +0300

    Scripted auto-commit on change (2020-11-23 20:24:06) by gitwatch.sh

--- src/main/java/bmstu/Test.java
+++ src/main/java/bmstu/Test.java
@@ -10,6 +10,30 @@ public class Test {
     private final String EXPECTED_RESULT = "expectedResult";
     private final String PARAMS = "params";
 
+    public String getTestName() {
+        return testName;
+    }
+
+    public void setTestName(String testName) {
+        this.testName = testName;
+    }
+
+    public String getExpRes() {
+        return expRes;
+    }
+
+    public void setExpRes(String expRes) {
+        this.expRes = expRes;
+    }
+
+    public ArrayList<Integer> getParams() {
+        return params;
+    }
+
+    public void setParams(ArrayList<Integer> params) {
+        this.params = params;
+    }
+
     @JsonProperty(TEST_NAME)
     private String testName;
 
