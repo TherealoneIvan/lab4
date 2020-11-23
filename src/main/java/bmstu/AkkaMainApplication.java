1645657ee629a54fbc0ac527a6d0c7840f398bd9
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-23 04:35:01 +0300

    Scripted auto-commit on change (2020-11-23 04:35:01) by gitwatch.sh

--- src/main/java/bmstu/AkkaMainApplication.java
+++ src/main/java/bmstu/AkkaMainApplication.java
@@ -45,6 +45,7 @@ public class AkkaMainApplication extends AllDirectives {
     private Route createRoute(ActorSystem system , ActorRef router){
          Route route =
                  post(()->entity(Jackson.unmarshaller(JavaScriptFunctionStore.class), msg -> {
+                     System.out.println("debug message 1");
                      router.tell(msg , router);
                      return complete("Test started!");
                  })).orElse(
